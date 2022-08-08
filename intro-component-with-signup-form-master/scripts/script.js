b7796b04c3e742d25496d6894c20424e1bd32e13
Kaio Brenner <kaiobrenner460@protonmail.com>
2022-08-07 22:05:57 -0300

    Update script.js

--- intro-component-with-signup-form-master/scripts/script.js
+++ intro-component-with-signup-form-master/scripts/script.js
@@ -13,10 +13,28 @@ function enviar(){
     let errorTxt2 = document.getElementById("error-txt2")
     let errorTxt3 = document.getElementById("error-txt3")
     let errorTxt4 = document.getElementById("error-txt4")
-    let inputs = document.querySelectorAll(".inputs")
 
+    if(fName.value.length > 0){
+        errorI1.style.cssText = "display:none; top: -40px;"
+        errorTxt1.style.cssText = "display:none; top:-17px;"
+        fName.style.cssText = "margin-bottom: 20px; border-color: #00000054;"
+    }else{
+        errorI1.style.cssText = "display:block; top: -40px;"
+        errorTxt1.style.cssText = "display:block; top:-17px;"
+        fName.style.cssText = "margin-bottom: 0px; border-color: red;"
+    }
 
-    if((fName.value.length > 0) && (lName.value.length > 0 ) && ((usuario.length >=1) &&
+    if(lName.value.length > 0 ){
+        errorI2.style.cssText = "display:none; top: -40px;"
+        errorTxt2.style.cssText = "display:none; top:-17px;"
+        lName.style.cssText= "margin-bottom: 20px; border-color: #00000054;" 
+    }else{
+        errorI2.style.cssText = "display:block; top: -40px;"
+        errorTxt2.style.cssText = "display:block; top:-17px;"
+        lName.style.cssText= "margin-bottom: 0px; border-color: red;" 
+    }
+
+    if((usuario.length >=1) &&
     (dominio.length >=3) &&
     (usuario.search("@")==-1) &&
     (dominio.search("@")==-1) &&
@@ -24,41 +42,24 @@ function enviar(){
     (dominio.search(" ")==-1) &&
     (dominio.search(".")!=-1) &&
     (dominio.indexOf(".") >=1)&&
-    (dominio.lastIndexOf(".") < dominio.length - 1)) && (senha.value.length > 0)){
-        errorI1.style.cssText = "display:none; top: -40px;"
-        errorI2.style.cssText = "display:none; top: -40px;"
+    (dominio.lastIndexOf(".") < dominio.length - 1)){
         errorI3.style.cssText = "display:none; top: -40px;"
-        errorI4.style.cssText = "display:none; top: -40px;"
-        errorTxt1.style.cssText = "display:none; top:-17px;"
-        errorTxt2.style.cssText = "display:none; top:-17px;"
         errorTxt3.style.cssText = "display:none; top:-17px;"
-        errorTxt4.style.cssText = "display:none; top:-17px;"
-        fName.style.cssText = "margin-bottom: 20px; border-color: #00000054;"
-        lName.style.cssText= "margin-bottom: 20px; border-color: #00000054;" 
         email.style.cssText = "margin-bottom: 20px; border-color: #00000054;"
+    }else{
+        errorI3.style.cssText = "display:block; top: -40px;"
+        errorTxt3.style.cssText = "display:block; top:-17px;"
+        email.style.cssText = "margin-bottom: 0px; border-color: red;"
+    }
+    
+    if(senha.value.length > 0){
+        errorI4.style.cssText = "display:none; top: -40px;"
+        errorTxt4.style.cssText = "display:none; top:-17px;"
         senha.style.cssText = "margin-bottom: 20px; border-color: #00000054;"
     }else{
-        errorI1.style.cssText = "display:block; top: -40px;"
-        errorI2.style.cssText = "display:block; top: -40px;"
-        errorI3.style.cssText = "display:block; top: -40px;"
         errorI4.style.cssText = "display:block; top: -40px;"
-        errorTxt1.style.cssText = "display:block; top:-17px;"
-        errorTxt2.style.cssText = "display:block; top:-17px;"
-        errorTxt3.style.cssText = "display:block; top:-17px;"
         errorTxt4.style.cssText = "display:block; top:-17px;"
-        fName.style.cssText = "margin-bottom: 0px; border-color: red;"
-        lName.style.cssText= "margin-bottom: 0px; border-color: red;" 
-        email.style.cssText = "margin-bottom: 0px; border-color: red;"
         senha.style.cssText = "margin-bottom: 0px; border-color: red;"
     }
 }
 
-/*(!((usuario.length >=1) &&
-    (dominio.length >=3) &&
-    (usuario.search("@")==-1) &&
-    (dominio.search("@")==-1) &&
-    (usuario.search(" ")==-1) &&
-    (dominio.search(" ")==-1) &&
-    (dominio.search(".")!=-1) &&
-    (dominio.indexOf(".") >=1)&&
-    (dominio.lastIndexOf(".") < dominio.length - 1)))*/
